c9206a651055480de0bc32fe74934f2e53224b48
Kevin Herron <kevinherron@gmail.com>
2015-03-07 07:40:15 -0800

    Rename and move AttributeResponse to structs package

--- enip-client/src/main/java/com/digitalpetri/enip/cip/services/GetAttributeListService.java
+++ enip-client/src/main/java/com/digitalpetri/enip/cip/services/GetAttributeListService.java
@@ -2,13 +2,14 @@ package com.digitalpetri.enip.cip.services;
 
 import com.digitalpetri.enip.cip.CipResponseException;
 import com.digitalpetri.enip.cip.epath.EPath.PaddedEPath;
+import com.digitalpetri.enip.cip.structs.AttributeResponse;
 import com.digitalpetri.enip.cip.structs.MessageRouterRequest;
 import com.digitalpetri.enip.cip.structs.MessageRouterResponse;
 import io.netty.buffer.ByteBuf;
 import io.netty.buffer.Unpooled;
 import io.netty.util.ReferenceCountUtil;
 
-public class GetAttributeListService implements CipService<GetAttributeListService.Attribute[]> {
+public class GetAttributeListService implements CipService<AttributeResponse[]> {
 
     public static final int SERVICE_CODE = 0x03;
 
@@ -34,7 +35,7 @@ public class GetAttributeListService implements CipService<GetAttributeListServi
     }
 
     @Override
-    public Attribute[] decodeResponse(ByteBuf buffer) throws CipResponseException, PartialResponseException {
+    public AttributeResponse[] decodeResponse(ByteBuf buffer) throws CipResponseException, PartialResponseException {
         MessageRouterResponse response = MessageRouterResponse.decode(buffer);
 
         if (response.getGeneralStatus() == 0x00) {
@@ -56,10 +57,10 @@ public class GetAttributeListService implements CipService<GetAttributeListServi
         }
     }
 
-    private Attribute[] decode(ByteBuf buffer) {
+    private AttributeResponse[] decode(ByteBuf buffer) {
         int count = buffer.readUnsignedShort();
 
-        Attribute[] attributes = new Attribute[count];
+        AttributeResponse[] attributeResponses = new AttributeResponse[count];
 
         for (int i = 0; i < count; i++) {
             int id = buffer.readUnsignedShort();
@@ -68,34 +69,10 @@ public class GetAttributeListService implements CipService<GetAttributeListServi
                     buffer.readSlice(attributeSizes[i]).copy() :
                     Unpooled.EMPTY_BUFFER;
 
-            attributes[i] = new Attribute(id, status, data);
+            attributeResponses[i] = new AttributeResponse(id, status, data);
         }
 
-        return attributes;
-    }
-
-    public static final class Attribute {
-        private final int id;
-        private final int status;
-        private final ByteBuf data;
-
-        public Attribute(int id, int status, ByteBuf data) {
-            this.id = id;
-            this.status = status;
-            this.data = data;
-        }
-
-        public int getId() {
-            return id;
-        }
-
-        public int getStatus() {
-            return status;
-        }
-
-        public ByteBuf getData() {
-            return data;
-        }
+        return attributeResponses;
     }
 
 }
---
+++ enip-client/src/main/java/com/digitalpetri/enip/cip/structs/AttributeResponse.java
@@ -0,0 +1,29 @@
+package com.digitalpetri.enip.cip.structs;
+
+import io.netty.buffer.ByteBuf;
+
+public final class AttributeResponse {
+    
+    private final int id;
+    private final int status;
+    private final ByteBuf data;
+
+    public AttributeResponse(int id, int status, ByteBuf data) {
+        this.id = id;
+        this.status = status;
+        this.data = data;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public ByteBuf getData() {
+        return data;
+    }
+
+}
