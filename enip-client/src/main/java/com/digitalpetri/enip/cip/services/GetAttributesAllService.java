d485b10f60150d78a1a42d7bb8d4db8aa836250f
Kevin Herron <kevinherron@gmail.com>
2015-03-07 17:33:55 -0800

    Implement Get_Attributes_All

---
+++ enip-client/src/main/java/com/digitalpetri/enip/cip/services/GetAttributesAllService.java
@@ -0,0 +1,41 @@
+package com.digitalpetri.enip.cip.services;
+
+import com.digitalpetri.enip.cip.CipResponseException;
+import com.digitalpetri.enip.cip.epath.EPath.PaddedEPath;
+import com.digitalpetri.enip.cip.structs.MessageRouterRequest;
+import com.digitalpetri.enip.cip.structs.MessageRouterResponse;
+import io.netty.buffer.ByteBuf;
+
+public class GetAttributesAllService implements CipService<ByteBuf> {
+
+    public static final int SERVICE_CODE = 0x01;
+
+    private final PaddedEPath requestPath;
+
+    public GetAttributesAllService(PaddedEPath requestPath) {
+        this.requestPath = requestPath;
+    }
+
+    @Override
+    public void encodeRequest(ByteBuf buffer) {
+        MessageRouterRequest request = new MessageRouterRequest(
+                SERVICE_CODE,
+                requestPath,
+                byteBuf -> {}
+        );
+
+        MessageRouterRequest.encode(request, buffer);
+    }
+
+    @Override
+    public ByteBuf decodeResponse(ByteBuf buffer) throws CipResponseException, PartialResponseException {
+        MessageRouterResponse response = MessageRouterResponse.decode(buffer);
+
+        if (response.getGeneralStatus() == 0x00) {
+            return response.getData();
+        } else {
+            throw new CipResponseException(response.getGeneralStatus(), response.getAdditionalStatus());
+        }
+    }
+
+}
