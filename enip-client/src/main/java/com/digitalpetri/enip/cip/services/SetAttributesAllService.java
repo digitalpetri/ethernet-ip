c0e41c2b1039134f6b5f4f5b7d70cbb174437c09
Kevin Herron <kevinherron@gmail.com>
2015-03-07 17:34:15 -0800

    Implement Set_Attributes_All

---
+++ enip-client/src/main/java/com/digitalpetri/enip/cip/services/SetAttributesAllService.java
@@ -0,0 +1,45 @@
+package com.digitalpetri.enip.cip.services;
+
+import java.util.function.Consumer;
+
+import com.digitalpetri.enip.cip.CipResponseException;
+import com.digitalpetri.enip.cip.epath.EPath.PaddedEPath;
+import com.digitalpetri.enip.cip.structs.MessageRouterRequest;
+import com.digitalpetri.enip.cip.structs.MessageRouterResponse;
+import io.netty.buffer.ByteBuf;
+
+public class SetAttributesAllService implements CipService<Void> {
+
+    public static final int SERVICE_CODE = 0x02;
+
+    private final PaddedEPath requestPath;
+    private final Consumer<ByteBuf> attributeEncoder;
+
+    public SetAttributesAllService(PaddedEPath requestPath, Consumer<ByteBuf> attributeEncoder) {
+        this.requestPath = requestPath;
+        this.attributeEncoder = attributeEncoder;
+    }
+
+    @Override
+    public void encodeRequest(ByteBuf buffer) {
+        MessageRouterRequest request = new MessageRouterRequest(
+                SERVICE_CODE,
+                requestPath,
+                attributeEncoder
+        );
+
+        MessageRouterRequest.encode(request, buffer);
+    }
+
+    @Override
+    public Void decodeResponse(ByteBuf buffer) throws CipResponseException, PartialResponseException {
+        MessageRouterResponse response = MessageRouterResponse.decode(buffer);
+
+        if (response.getGeneralStatus() == 0x00) {
+            return null;
+        } else {
+            throw new CipResponseException(response.getGeneralStatus(), response.getAdditionalStatus());
+        }
+    }
+
+}
