4c274756004150bc97c8c88098548e800b17c84f
Sushma Sharma <susharma@C02XV11TJGH6.local>
2019-12-01 16:37:29 -0700

    WriteTagFragmentedService-implementation

---
+++ logix-services/src/main/java/com/digitalpetri/enip/logix/services/WriteTagFragmentedService.java
@@ -0,0 +1,88 @@
+package com.digitalpetri.enip.logix.services;
+
+import java.util.function.Consumer;
+
+import com.digitalpetri.enip.cip.CipResponseException;
+import com.digitalpetri.enip.cip.epath.EPath.PaddedEPath;
+import com.digitalpetri.enip.cip.services.CipService;
+import com.digitalpetri.enip.cip.structs.MessageRouterRequest;
+import com.digitalpetri.enip.cip.structs.MessageRouterResponse;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.util.ReferenceCountUtil;
+
+public class WriteTagFragmentedService implements CipService<ByteBuf> {
+
+    public static final int SERVICE_CODE = 0x53;
+
+    private final Consumer<ByteBuf> dataEncoder = this::encode;
+
+    private final PaddedEPath requestPath;
+    private final int elementCount;
+    private final boolean structured;
+    private final int tagType;
+    private final ByteBuf data;
+    private volatile int offset = 0;
+
+    public WriteTagFragmentedService(PaddedEPath requestPath, boolean structured, int tagType, ByteBuf data) {
+
+        this(requestPath, structured, tagType, 1, data);
+    }
+
+    public WriteTagFragmentedService(PaddedEPath requestPath,
+            boolean structured,
+            int tagType,
+            int elementCount,
+            ByteBuf data) {
+
+        this.requestPath = requestPath;
+        this.structured = structured;
+        this.tagType = tagType;
+        this.elementCount = elementCount;
+        this.data = data;
+    }
+
+    @Override
+    public void encodeRequest(ByteBuf buffer) {
+
+        MessageRouterRequest request = new MessageRouterRequest(
+                SERVICE_CODE,
+                requestPath,
+                dataEncoder);
+
+        MessageRouterRequest.encode(request, buffer);
+    }
+
+    private void encode(ByteBuf buffer) {
+
+        if (structured) {
+            buffer.writeByte(0xA0).writeByte(0x02);
+        }
+
+        buffer.writeShort(tagType);
+        buffer.writeShort(elementCount);
+        buffer.writeInt(offset);
+        buffer.writeBytes(data);
+
+    }
+
+    @Override
+    public ByteBuf decodeResponse(ByteBuf buffer) throws CipResponseException, PartialResponseException {
+
+        MessageRouterResponse response = MessageRouterResponse.decode(buffer);
+
+        int generalStatus = response.getGeneralStatus();
+
+        try {
+            if (generalStatus == 0x00) {
+                return null;
+            } else {
+                throw new CipResponseException(generalStatus, response.getAdditionalStatus());
+            }
+        } finally {
+            ReferenceCountUtil.release(response.getData());
+        }
+
+    }
+
+}
