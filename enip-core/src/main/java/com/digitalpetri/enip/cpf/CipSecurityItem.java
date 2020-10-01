4d0c71000f140b6ce0e566907e37efe03baf6dd6
Kevin Herron <kevinherron@gmail.com>
2020-10-01 15:08:58 -0700

    ~ javadoc on CipSecurityItem

--- enip-core/src/main/java/com/digitalpetri/enip/cpf/CipSecurityItem.java
+++ enip-core/src/main/java/com/digitalpetri/enip/cpf/CipSecurityItem.java
@@ -22,18 +22,75 @@ public class CipSecurityItem extends CpfItem {
         this.ianaPortState = ianaPortState;
     }
 
+    /**
+     * Get the CIP Security Profiles supported by the device.
+     *
+     * <pre>
+     *  Bit 0       EtherNet/IP Integrity Profile
+     *  Bit 1       EtherNet/IP Confidentiality Profile
+     *  Bit 2       CIP Authorization Profile
+     *  Bit 3       CIP Integrity Profile
+     *  Bit 4-15    Reserved
+     * </pre>
+     *
+     * @return the CIP Security Profiles supported by the device.
+     */
     public int getSecurityProfiles() {
         return securityProfiles;
     }
 
+    /**
+     * Get the current state of the CIP Security Object.
+     *
+     * <pre>
+     *  0   Factory Default Configuration
+     *  1   Initial Commissioning In Progress
+     *  2   Configured
+     *  3   Incomplete Configuration
+     * </pre>
+     *
+     * @return the current state of the CIP Security Object.
+     */
     public int getCipSecurityState() {
         return cipSecurityState;
     }
 
+    /**
+     * Get the current state of the EtherNet/IP Security Object associated with the IP address
+     * where the request was received.
+     *
+     * <pre>
+     *  0   Factory Default Configuration
+     *  1   Configuration In Progress
+     *  2   Configured
+     * </pre>
+     *
+     * @return the current state of the EtherNet/IP Security Object associated with the IP address
+     * where the request was received.
+     */
     public int getEnipSecurityState() {
         return enipSecurityState;
     }
 
+    /**
+     * Get the current state, open or closed, for all EtherNet/IP related IANA ports Object
+     * associated with the IP address where the request was received.
+     * <p>
+     * 1 (TRUE) shall indicate that the corresponding port is open. If the bit is 0 (FALSE) the
+     * port is closed. Reserved bits shall be 0.
+     *
+     * <pre>
+     *  Bit 0    44818/tcp
+     *  Bit 1    44818/udp
+     *  Bit 2    2222/udp
+     *  Bit 3    2221/tcp
+     *  Bit 4    2221/udp
+     *  Bit 5-7  Reserved
+     * </pre>
+     *
+     * @return the current state, open or closed, for all EtherNet/IP related IANA ports Object
+     * associated with the IP address where the request was received.
+     */
     public int getIanaPortState() {
         return ianaPortState;
     }
